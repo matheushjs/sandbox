860736097a15de62967fb21c88efd52d82dda755
Matheus Saldanha <matheus.saldanha@usp.br>
2018-01-31 20:59:32 -0200

    First kernel module

---
+++ kernel_train/mymodule.c
@@ -0,0 +1,11 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void){
+	printk(KERN_ALERT "Hello world.\n");
+	return 0;
+}
+
+void cleanup_module(void){
+	printk(KERN_ALERT "Goodbye world.\n");
+}
