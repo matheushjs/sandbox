e086e3bd2c45aa7e825bf4706e73e17328d1026a
Matheus Saldanha <matheus.saldanha@usp.br>
2018-01-31 21:01:59 -0200

    Second kernel module

--- kernel_train/mymodule.c
+++ kernel_train/mymodule.c
@@ -1,11 +1,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-int init_module(void){
-	printk(KERN_ALERT "Hello world.\n");
+static int __init myinit(void){
+	printk(KERN_INFO "Hello, world\n");
 	return 0;
 }
 
-void cleanup_module(void){
-	printk(KERN_ALERT "Goodbye world.\n");
+static void __exit myexit(void){
+	printk(KERN_INFO "Goodbye, world\n");
 }
+
+module_init(myinit);
+module_exit(myexit);
