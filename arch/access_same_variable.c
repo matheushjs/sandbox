fd909172ad6af2edd4c4796811d0680b7a5f7889
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-11-28 20:49:55 -0200

    Change to stencil code

--- arch/access_same_variable.c
+++ arch/access_same_variable.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/* Utilizamos este código para analisar diferenças arquiteturais relativas
+ *   a códigos com dependência de dados.
+ *
+ * O algoritmo utilizado faz parte de uma classe de algoritmos comumente chamada
+ *   de "Stencil".
+ * Algoritmos de "Stencil" consistem em, dado uma matriz N-dimensional, cada elemento
+ *   dessa matriz recebe o valor de uma computação (e.g. soma) sobre elementos da mesma
+ *   matriz, sendo que esses elementos são acessados de acordo com um padrão.
+ *
+ * Em geral, algoritmos de Stencil recebem uma matriz de entrada e criam uma nova matriz
+ *   que será retornada ao usuário. No entanto, para analizar dependências de dados, aqui
+ *   realizamos a operação de stencil na própria matriz (no caso unidimensional, i.e. um
+ *   vetor), sem se preocupar com a corretude ou a funcionalidade do algoritmo.
+ *
+ * Neste código, a operação de stencil realizada é a seguinte:
+ *   vector[i] = (vector[i-2] + vector[i-1] + vector[i] + vector[i+1] + vector[i+2]) / 5
+ * Ou seja, cada elemento recebe a média dos 5 elementos que o circundam.
+ */
+
+#define N 10000
 
 int main(int argc, char *argv[]){
 	int i, j;
-	int *vector = malloc(sizeof(int) * 8);
+	int *vector = malloc(sizeof(int) * N);
 
 	// Inicializa vetor
-	for(i = 0; i < 8; i++){
-		vector[i] = 1;
+	for(i = 0; i < N; i++){
+		vector[i] = i;
 	}
 
 	// Realiza computação
-	for(i = 0; i < 50000000; i++){
-		for(j = 0; j < 4; j++){
-			vector[j] = vector[j] + vector[j+1] + vector[j+2] + vector[j+3];
-		}
+	for(j = 2; j < N-2; j++){
+		vector[j] = (vector[j-2] + vector[j-1] + vector[j] + vector[j+1] + vector[j+2]) / 5;
 	}
 
-	// Imprime vetor
-	for(i = 0; i < 4; i++){
-		printf("%d ", vector[i]);
+	// Imprime alguns elementos
+	for(i = 0; i < 8; i++){
+		printf("%d ", vector[8*i]);
 	}
 	printf("\n");
 
