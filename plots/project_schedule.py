0eb1967a2d92106382b5211965a62a016a5ffcfa
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-12-21 19:42:59 -0300

    Greatly improve gantt plot

--- plots/project_schedule.py
+++ plots/project_schedule.py
@@ -16,6 +16,7 @@ extraMargin = 0.5 # Top-most and bottom-most margins
 initDate    = dt.date(year=2020, month=2, day=1)
 
 # Increase size of labels. Set any of them to a wrong value, and the error will tell you what are valid values.
+# print(plt.rcParams.keys())
 plt.rcParams["xtick.labelsize"] = "large"
 plt.rcParams["ytick.labelsize"] = "large"
 
@@ -23,7 +24,12 @@ plt.rcParams["ytick.labelsize"] = "large"
 plt.rcParams["xtick.bottom"] = plt.rcParams["xtick.labelbottom"] = True
 plt.rcParams["xtick.top"]    = plt.rcParams["xtick.labeltop"] = True
 
+# Remove yticks
+plt.rcParams["ytick.minor.size"] = 0
+plt.rcParams["ytick.major.size"] = 0
+
 fig, ax = plt.subplots(figsize=(12, 6))
+ax.set_facecolor("#d7d7d7")
 
 # Add the horizontal bars
 for idx, label in enumerate(labels):
@@ -42,25 +48,35 @@ def date_tick(idx, date):
     else:
         return months[int(dmy[1]) - 1]
 
+fontproperties = {
+    "weight": "bold",
+    "size": 12
+}
+
 # Horizontal bars have heights on [0.6, 1.4], [1.6, 2.4], ... (depends on ywidth)
 ax.set_xlim(1, len(allDates) + 1)
-ax.set_xlabel("Months", fontsize=14, labelpad=14)
+ax.set_xlabel("Months", fontsize=14, fontweight="bold", labelpad=14)
 ax.set_xticks(np.arange(1, len(allDates)+1+0.1))
 ax.set_xticks(np.arange(1, len(allDates) + 0.1)+0.5, minor=True)
 ax.set_xticklabels([ ""                for idx, d in enumerate(allDates) ])
-ax.set_xticklabels([ date_tick(idx, d) for idx, d in enumerate(allDates) ], minor=True)
+ax.set_xticklabels([ date_tick(idx, d) for idx, d in enumerate(allDates) ], fontproperties, minor=True)
 
 # Y labels are positioned at [1, 2, 3, 4]...
 ax.set_ylim(0.5 - extraMargin, len(labels) + 0.5 + extraMargin)
 ax.set_yticks(np.arange(1, len(labels) + 0.1))
-ax.set_yticklabels(labels) # It is inverted later
+ax.set_yticklabels(labels, fontproperties) # It is inverted later
+ax.spines["left"].set_visible(False)
+ax.spines["right"].set_visible(False)
+
+# Set a faint grid along the x axis
 ax.grid(True, axis="x", c="k", ls="-", alpha=0.15, zorder=1)
 
 # Invert yaxis because bars were added from bottom to top
 ax.invert_yaxis()
 
-# The 'Months' label is better on top
+# The "Months" label is better on top
 ax.xaxis.set_label_position("top")
 
+plt.tight_layout()
 plt.savefig("project_schedule.png");
 plt.show()
