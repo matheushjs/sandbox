e2f601ed57e74fbbab8c1755e71165041791a87a
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-01-10 22:36:18 -0300

    Give the user more control variables

--- plots/project_schedule.py
+++ plots/project_schedule.py
@@ -14,6 +14,11 @@ times  = {
 ywidth      = 0.7 # Width of the horizontal bars
 extraMargin = 0.5 # Top-most and bottom-most margins
 initDate    = dt.date(year=2020, month=2, day=1)
+nMonths     = 12  # Number of months in the gantt chart
+minInterval = 1   # The minimal interval a task can use
+titleSize   = 20  # font size
+yTextPt     = 20  # font size
+xTextPt     = 16  # font size
 
 # Increase size of labels. Set any of them to a wrong value, and the error will tell you what are valid values.
 # print(plt.rcParams.keys())
@@ -36,30 +41,33 @@ for idx, label in enumerate(labels):
     ax.broken_barh(times[label], (idx + 1 - ywidth/2, ywidth), facecolors="#126262", zorder=2, edgecolor="black", linewidth=1.5)
 
 # Generate all months of the project
-allDates = [ initDate + dt.timedelta(days=31*i) for i in range(12) ]
+allDates = [ initDate + dt.timedelta(days=31*i) for i in range(nMonths) if i % minInterval == 0 ]
 
 # This function will generate the labels for the xticks
 # "idx" is the index of the xtick, and date is the date in the format "YYYY-MM-DD"
+SAVED_YEAR = ""
 def date_tick(idx, date):
+    global SAVED_YEAR
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     dmy = str(date).split("-")[::-1]
-    if int(dmy[1]) == 1 or idx == 0:
-        return months[int(dmy[1]) - 1] + " " + dmy[2]
+    if int(dmy[2]) != SAVED_YEAR:
+        SAVED_YEAR = int(dmy[2])
+        return dmy[2] + "\n" + months[int(dmy[1]) - 1]
     else:
         return months[int(dmy[1]) - 1]
 
 # Horizontal bars have heights on [0.6, 1.4], [1.6, 2.4], ... (depends on ywidth)
 ax.set_xlim(1, len(allDates) + 1)
-ax.set_xlabel("Months", fontsize=20, fontweight="bold", labelpad=14)
+ax.set_xlabel("Months", fontsize=titleSize, fontweight="bold", labelpad=14)
 ax.set_xticks(np.arange(1, len(allDates)+1+0.1))
 ax.set_xticks(np.arange(1, len(allDates) + 0.1)+0.5, minor=True)
 ax.set_xticklabels([ ""                for idx, d in enumerate(allDates) ])
-ax.set_xticklabels([ date_tick(idx, d) for idx, d in enumerate(allDates) ], { "weight": "bold", "size": 16 }, minor=True)
+ax.set_xticklabels([ date_tick(idx, d) for idx, d in enumerate(allDates) ], { "weight": "bold", "size": xTextPt }, minor=True)
 
 # Y labels are positioned at [1, 2, 3, 4]...
 ax.set_ylim(0.5 - extraMargin, len(labels) + 0.5 + extraMargin)
 ax.set_yticks(np.arange(1, len(labels) + 0.1))
-ax.set_yticklabels(labels, { "weight": "bold", "size": 20 }) # It is inverted later
+ax.set_yticklabels(labels, { "weight": "bold", "size": yTextPt }) # It is inverted later
 ax.spines["left"].set_visible(False)
 ax.spines["right"].set_visible(False)
 
