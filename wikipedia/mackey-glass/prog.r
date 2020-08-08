0bfd33b07e6f7533daafb74fed4370a0da8273fd
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-08-08 06:24:00 -0300

    Finish the mackey-glass equations

--- wikipedia/mackey-glass/prog.r
+++ wikipedia/mackey-glass/prog.r
@@ -47,10 +47,10 @@ rotZ = function(data, theta){
 }
 
 chaotic = function(){
-	params = list(gamma=0.1, beta=0.2, theta=2, n=10, tau=20);
+	params = list(gamma=0.1, beta=0.2, theta=2, n=10, tau=22);
 	P0 = 0.1;
 	BY = 0.1;
-	times = seq(-params$tau, 800, by=BY);
+	times = seq(-params$tau, 1000, by=BY);
 
 	out = dede(P0, times, dP, parms=params);
 
@@ -71,9 +71,68 @@ chaotic = function(){
 	for(zi in z)
 		cols = c(cols, palette[round(colN * (zi - min(z)) / (max(z) - min(z)) )]);
 
-	plot3d(data3, type="l", axes=F, xlab="", ylab="", zlab="", col=cols, lwd=5);
+	plot3d(data3, type="l", axes=F, xlab="", ylab="", zlab="", col=cols, lwd=2);
 
-	# snapshot3d("mackey-glass20.png");
+	snapshot3d(paste("mackey-glass", params$tau, ".png", sep=""));
+}
+
+simple = function(){
+	params = list(gamma=0.1, beta=0.2, theta=2, n=10, tau=12);
+	P0 = 0.1;
+	BY = 0.1;
+	times = seq(-params$tau, 800, by=BY);
+
+	out = dede(P0, times, dP, parms=params);
+	plot(out[,1], out[,2] / params$theta, type="l", lwd=2);
+
+	data2 = embedd(out[,2], m=2, d=params$tau / BY);
+	data3 = embedd(out[,2], m=3, d=params$tau / BY);
+	data3 = data3[-c(1:1000),]
+
+	#plot(out / params$theta, which=1, ylim=c(0, 1.5), lwd=2);
+	#plot(data3[,1:2], type="p", col=sequential_hcl("Teal", n=round(nrow(data3)*1.5)));
+
+	data3 = rotZ(rotY(rotX(data3, 15), 15), 0);
+
+	z = rotZ(rotY(rotX(data3, 90), 90), 90)[,3];
+	colN = 1000;
+	palette = sequential_hcl("Teal", n=colN*1.3)
+	palette = palette[1:colN];
+	cols = NULL;
+	for(zi in z)
+		cols = c(cols, palette[round(colN * (zi - min(z)) / (max(z) - min(z)) )]);
+
+	plot3d(data3, type="l", axes=F, xlab="", ylab="", zlab="", col=cols, lwd=3);
+
+	# snapshot3d("mackey-glass12.png");
+}
+
+timeseries = function(tau=6){
+	graphics.off();
+
+	params = list(gamma=0.1, beta=0.2, theta=2, n=10, tau=20);
+	P0 = 0.1;
+	BY = 0.1;
+	times = seq(-params$tau, 800, by=BY);
+
+	out = dede(P0, times, dP, parms=params);
+
+	dev.new(width=8, height=4);
+	plot(out[,1], out[,2] / params$theta, type="l", ylab=expression(P / theta), xlab="time (days)", lwd=2);
+	savePlot("series20.png");
+
+	params = list(gamma=0.1, beta=0.2, theta=2, n=10, tau=6);
+	P0 = 0.1;
+	BY = 0.1;
+	times = seq(-params$tau, 800, by=BY);
+
+	out = dede(P0, times, dP, parms=params);
+
+	dev.new(width=8, height=4);
+	plot(out[,1], out[,2] / params$theta, type="l", ylab=expression(P / theta), xlab="time (days)", lwd=2);
+	savePlot("series6.png");
 }
 
 chaotic();
+#simple();
+#timeseries();
