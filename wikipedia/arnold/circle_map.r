b4dbc4f337bf0ac15fd18a4e5ca0f33dd51525cb
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-08-18 17:35:00 -0300

    Begin circle_map.r for prototypes

---
+++ wikipedia/arnold/circle_map.r
@@ -0,0 +1,14 @@
+
+
+f = function(theta, omega, K) theta + omega + (K / 2 / pi) * sin(2 * pi * theta);
+
+x = runif(min=0, max=1, n=1);
+result = x;
+for(i in 1:100000){
+	x = f(x, omega=1/3, K=1.7);
+	result = c(result, x);
+}
+
+par(mfrow=c(1, 2));
+plot(result %% 1);
+plot( (result / (1 : length(result)))[-c(1:10)] , type="l");
